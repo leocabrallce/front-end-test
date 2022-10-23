37499e0c0a57d03aa7b2f3f2e393d234f9adf54d
Leonardo Cabral <leocabrallce@gmail.com>
2022-10-23 03:02:08 -0300

    storybook config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#685DC5",
+        secondary: "#DADADA",
+      }
+    },
   },
   plugins: [],
 }
