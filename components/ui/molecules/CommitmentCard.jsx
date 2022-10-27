30c96d2b7fd3084135afd7dc108e565388fc2393
Leonardo Cabral <leocabrallce@gmail.com>
2022-10-27 00:43:56 -0300

    commitment card style

--- components/ui/molecules/CommitmentCard.jsx
+++ components/ui/molecules/CommitmentCard.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 
 export function CommitmentCard({ title, primaryText, secondaryText }) {
 	return (
-		<div>
+		<div className='border border-solid border-secondary-200 rounded px-6 py-8'>
 			<div>
-				<div>Study commitment</div>
-				<div>3 hours / day</div>
+				<div className='text-primary mb-2 text-sm'>Study commitment</div>
+				<div className='text-secondary-400 text-2xl font-extralight'>3 hours / day</div>
 			</div>
-			<hr className="divide-x" />
-			<div>
+			<hr className="divide-x mt-6 mb-4 w-8" />
+			<p className='text-secondary-400 font-extralight'>
 				You will complete 15 modules to graduate. Daily classes are 3
 				hours, plus coursework to complete in your own time.
-			</div>
+			</p>
 		</div>
 	)
 }
